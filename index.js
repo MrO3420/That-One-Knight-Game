e038c51aa9dced6612db08d747a5ab7f09f48b27
MrO3420 <omason632@gmail.com>
2026-05-14 18:00:01 -0400

    Working on the game

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1990
-canvas.height = 2000
+canvas.width = 1899
+canvas.height = 925
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+draw() {
+c.fillStyle = 'red'
+c.fillRect(this.position.x, this.position.y, 50, 150)
+
+ }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+player.draw()
+
+const enemy = new Sprite({
+x: 1840,
+y: 0
+})
+
+enemy.draw()
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    console.log('move')
+}
+
+animate()
