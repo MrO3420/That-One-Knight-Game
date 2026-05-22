aa6a7439d7d94b9372ba939966a61204db470a4e
MrO3420 <omason632@gmail.com>
2026-05-22 16:53:31 -0400

    Working on my game

--- index.js
+++ index.js
@@ -6,21 +6,30 @@ canvas.height = 925
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 1
+
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
 draw() {
 c.fillStyle = 'red'
-c.fillRect(this.position.x, this.position.y, 50, 150)
+c.fillRect(this.position.x, this.position.y, 50, this.height)
 
  }
 
  update() {
    this.draw()
-   this.position.y += 10
+   this.position.x += this.velocity.x
+   this.position.y += this.velocity.y
+
+
+   if (this.position.y + this.height +this.velocity.y >= canvas.height) {
+    this.velocity.y = 0
+   }else this.velocity.y += gravity
  }
 }
 
@@ -41,8 +50,8 @@ const player = new Sprite({
 
 const enemy  = new Sprite({
     position: {
-    x: 400,
-    y: 100
+    x: 1850,
+    y: 0
     },
     velocity: {
        x: 0,
@@ -54,11 +63,58 @@ const enemy  = new Sprite({
 
 console.log(player)
 
-function animate() {
-    window.requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    enemy.update()
+const keys = {
+   a: {
+     pressed: false
+   },
+   d: {
+     pressed: false
+   }
+
 }
 
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+if (keys.a.pressed) {
+    player.velocity.x = -1
+ } else if (keys.d.pressed) {
+    player.velocity.x = 1
+  }
+}
+
+
+
+
 animate()
+window.addEventListener('keydown', (event) => {
+   switch (event.key) {
+     case 'd':
+        keys.d.pressed = true
+        break
+
+        case 'a':
+        keys.a.pressed = true
+        break
+   }
+  console.log(event.key);
+})
+
+
+
+window.addEventListener('keyup', (event) => {
+   switch (event.key) {
+    case 'd':
+        keys.d.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+   }
+  console.log(event.key);
+})
